6f0522f17c018dc5c5fe27b3c626b304d4ba247f
Ricardo <ricardoac99@gmail.com>
2022-10-27 21:31:23 -0600

    Add sound button functionality. Fix a bug in the sound mode - Declares btnSnd variable. - EventListeners in buttons now validate if calculator is ON before calling the functions. - Fixed and cleaned turnOnCalculator and turnOffCalculator functions. - In the toggleLed function, replaced the toggle method for add or remove class. - The toggleLed function now uses the power-light and sound-light classes. - Fixed a bug where the sound led turned ON and sound mode button icon changed incorrectly     when the user turned OFF the calculator and sound mode was OFF. - Added and edited some comments.

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,12 @@
 
 // *************************** VARIABLES ******************************
 let calculatorIsOn = false;
-let soundIsOn = true;
+let soundIsOn = false;
 const calculator = document.querySelector("#calculator-body");
 const divScreen = calculator.querySelector("#screen");
 const btnOn = calculator.querySelector("#btn-on");
 const btnOff = calculator.querySelector("#btn-off");
+const btnSnd = calculator.querySelector("#btn-sound");
 const divPwrLed = calculator.querySelector("#power-led");
 const divSndLed = calculator.querySelector("#sound-led");
 const audioBeep = calculator.querySelector("#audio-beep");
@@ -14,29 +15,48 @@ const audioBeep = calculator.querySelector("#audio-beep");
 
 // ********************* FUNCTION DECLARATIONS ************************
 function turnOnCalculator() {
-    /* If calculator isn´t ON: Turns it on, turns on the screen,
-    turns the leds on and plays the beep */
-    if (!calculatorIsOn) {
-        calculatorIsOn = true;
-        playBeep();
-        toggleLed(divPwrLed, "power-on");
-        toggleLed(divSndLed, "sound-on");
-        toggleScreen();        
-        console.log(calculatorIsOn);
-    }    
+    //Turns calculator ON, turns power led ON,
+    //turns screen ON, turns sound mode ON and plays the beep.          
+    calculatorIsOn = true;
+    console.log("Calculator " + calculatorIsOn);
+    toggleLed(calculatorIsOn, divPwrLed, "power-light");        
+    toggleScreen();
+    toggleSound();
+    playBeep();                 
 }
 
-// Puts initial 0 or turns off the screen
+// Turns ON the screen (puts initial 0) or turns it OFF.
 function toggleScreen() {
     calculatorIsOn ? divScreen.textContent = "0." : divScreen.textContent = "";
 }
 
-// Toggles ON or OFF the received by argument led
-function toggleLed(led, ledStatus) {
-    led.classList.toggle(ledStatus);
+// Toggles ON or OFF the desired led.
+function toggleLed(status, led, ledClass) {
+    if (status) {
+        led.classList.add(ledClass);
+    } else {
+        led.classList.remove(ledClass);
+    }
 }
 
-// If sound mode is activated, plays the beep sound
+// If the calculator is ON, toggles sound mode (toggles sound led and changes icon of sound button).
+// If the calculator has just turned OFF, turns sound mode OFF (turns sound led OFF and changes icon of sound button).
+function toggleSound() {
+    if (calculatorIsOn) {
+        soundIsOn ? soundIsOn = false : soundIsOn = true;
+        soundIsOn ? btnSnd.textContent = "volume_off" : btnSnd.textContent = "volume_up";
+        toggleLed(soundIsOn, divSndLed, "sound-light");
+        console.log("Sound " + soundIsOn);
+
+    } else {
+        soundIsOn = false;
+        btnSnd.textContent = "volume_off";
+        toggleLed(soundIsOn, divSndLed, "sound-light");
+        console.log("Sound " + soundIsOn);
+    } 
+}
+
+// If sound mode is ON, plays the beep sound.
 function playBeep() {
     if (soundIsOn) {
         audioBeep.currentTime = 0;
@@ -45,25 +65,34 @@ function playBeep() {
 }
 
 function turnOffCalculator() {
-    /* If calculator is ON: Turns it off, turns off the screen,
-    and turns the leds off */
-    if (calculatorIsOn) {
-        calculatorIsOn = false;        
-        toggleLed(divPwrLed, "power-on");
-        toggleLed(divSndLed, "sound-on"); 
-        toggleScreen();      
-        console.log(calculatorIsOn);
-    }
+    // Turns calculator OFF, turns the power led OFF, turns the screen OFF, 
+    // and turns sound mode OFF.
+    calculatorIsOn = false;
+    console.log("Calculator " + calculatorIsOn);       
+    toggleLed(calculatorIsOn, divPwrLed, "power-light");                  
+    toggleScreen(); 
+    toggleSound();            
 }
 
 
 // ********************* CALCULATOR UI BUTTONS ************************
-// ON button
+// ON button: Turns on calculator if it is OFF.
 btnOn.addEventListener("click", function() {
-    turnOnCalculator();
+    if (!calculatorIsOn) {
+        turnOnCalculator();
+    }
 });
 
-// OFF button
+// OFF button: Turns off calculator if it is ON
 btnOff.addEventListener("click", function() {
-    turnOffCalculator();
+    if (calculatorIsOn) {
+        turnOffCalculator();
+    }
+});
+
+// Sound button: Toggles sound if calculator is ON
+btnSnd.addEventListener("click", function() {
+    if (calculatorIsOn) {
+        toggleSound();
+    }
 });
