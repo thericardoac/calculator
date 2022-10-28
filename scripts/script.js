8ee23b283fdda1840e0a622dbfd37eede5175d63
Ricardo <ricardoac99@gmail.com>
2022-10-27 18:53:40 -0600

    Add OFF functionality. Renamed startScreen function - Declared btnOff variable - Declared the turnOffCalculator function. - Renamed startScreen function to toggleScreen. - startScreen function puts 0 on screen when turned ON, clears it when turned OFF. - Added EventListener to UI OFF button.

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ let soundIsOn = true;
 const calculator = document.querySelector("#calculator-body");
 const divScreen = calculator.querySelector("#screen");
 const btnOn = calculator.querySelector("#btn-on");
+const btnOff = calculator.querySelector("#btn-off");
 const divPwrLed = calculator.querySelector("#power-led");
 const divSndLed = calculator.querySelector("#sound-led");
 const audioBeep = calculator.querySelector("#audio-beep");
@@ -13,21 +14,21 @@ const audioBeep = calculator.querySelector("#audio-beep");
 
 // ********************* FUNCTION DECLARATIONS ************************
 function turnOnCalculator() {
-    /* If calculator isn´t ON: Turns it on, starts the screen,
+    /* If calculator isn´t ON: Turns it on, turns on the screen,
     turns the leds on and plays the beep */
     if (!calculatorIsOn) {
-        calculatorIsOn = true;        
-        startScreen();
+        calculatorIsOn = true;
+        playBeep();
         toggleLed(divPwrLed, "power-on");
         toggleLed(divSndLed, "sound-on");
-        playBeep();
+        toggleScreen();        
         console.log(calculatorIsOn);
     }    
 }
 
-// Puts initial 0 on the screen
-function startScreen() {
-    divScreen.textContent = "0.";
+// Puts initial 0 or turns off the screen
+function toggleScreen() {
+    calculatorIsOn ? divScreen.textContent = "0." : divScreen.textContent = "";
 }
 
 // Toggles ON or OFF the received by argument led
@@ -43,9 +44,26 @@ function playBeep() {
     }
 }
 
+function turnOffCalculator() {
+    /* If calculator is ON: Turns it off, turns off the screen,
+    and turns the leds off */
+    if (calculatorIsOn) {
+        calculatorIsOn = false;        
+        toggleLed(divPwrLed, "power-on");
+        toggleLed(divSndLed, "sound-on"); 
+        toggleScreen();      
+        console.log(calculatorIsOn);
+    }
+}
+
 
 // ********************* CALCULATOR UI BUTTONS ************************
 // ON button
 btnOn.addEventListener("click", function() {
     turnOnCalculator();
+});
+
+// OFF button
+btnOff.addEventListener("click", function() {
+    turnOffCalculator();
 });
